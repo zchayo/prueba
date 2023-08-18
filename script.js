6c5829047b3f0c026a7c677f45086ac3a7a8678e
zchayo <112364736+zchayo@users.noreply.github.com>
2023-08-18 10:18:26 -0600

    Ejemplo responsive 2

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
 $(() => {
-  const navigation = [
-    { id: 1, text: 'Products', icon: 'product', url: 'view.tablas.html' },
-    { id: 2, text: 'Sales', icon: 'money', url: 'view.graficas.html' },
-    { id: 3, text: 'Customers', icon: 'group', url: 'https://www.mercadolibre.com.mx/'},
-    { id: 4, text: 'Employees', icon: 'card' },
-    { id: 5, text: 'Reports', icon: 'chart' },
-  ];
 
-  $('#responsive-box').dxResponsiveBox({
-    rows: [
-      { ratio: 1 },
-      { ratio: 8,  shrink: 20 },
-    //  { ratio: 10, screen: 'sm' }
-      // { ratio: 16, screen: 'sm' }
-    ],
-    cols: [
-      { ratio: 1 },
-      { ratio: 4, screen: 'lg' },
-    ],
-    singleColumnScreen: 'sm',
-    screenByWidth(width) {
-      return (width < 700) ? 'sm' : 'lg';
-    },
-  });
+    const cajaPequeña = document.querySelector(".caja-1");
+    const cajaGrande = document.querySelector(".caja-2");
+    const boton = document.querySelector(".boton")
+    let esPantallaChica = false
 
-  $('#list').dxList({
-    dataSource: navigation,
-    hoverStateEnabled: true,
-    focusStateEnabled: true,
-    activeStateEnabled: true,
-    onItemClick(e) {
+    if (window.innerWidth <= 768) {
 
-      console.log(e)
-      $('#mi-iframe').attr('src', e.itemData.url);
-    }
+        esPantallaChica = true;
+        // Código para dispositivos móviles
+        cajaPequeña.style.display = "none";
+        cajaGrande.style.flexGrow = "2";
+        boton.style.display = 'block';
+
+        $('#boton').dxButton({
+            stylingMode: 'outlined',
+            text: 'Menu',
+            type: 'normal',
+            icon: 'menu',
+            onClick() {
+                cajaGrande.style.display = "none";
+                cajaPequeña.style.display = "block";
+                cajaPequeña.style.flexGrow = "2";
+            },
+        });
+    };
 
 
-  });
+
+
+
+    const navigation = [
+        { id: 1, text: 'Products', icon: 'product', url: 'view.tablas.html' },
+        { id: 2, text: 'Sales', icon: 'money', url: 'view.graficas.html' },
+        { id: 3, text: 'Customers', icon: 'group' },
+        { id: 4, text: 'Employees', icon: 'card' },
+        { id: 5, text: 'Reports', icon: 'chart' },
+    ];
+
+    $('#list').dxList({
+        dataSource: navigation,
+        hoverStateEnabled: true,
+        focusStateEnabled: true,
+        activeStateEnabled: true,
+        searchEnabled: true,
+        searchExpr: 'text',
+        onItemClick(e) {
+            console.log(e)
+            if (esPantallaChica){
+                cajaPequeña.style.display = "none";
+                cajaGrande.style.display = "flex";
+                cajaGrande.style.flexGrow = "2";
+            }
+
+            $('#mi-iframe').attr('src', e.itemData.url);
+
+        }
+    });
 
 
 
